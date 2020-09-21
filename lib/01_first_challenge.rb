1c36f9d7a7af75bf70445aa4fdc7db602cc8ea78
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-21 16:32:43 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,3 +1,10 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {:montague => {
+     :patriarch {
+       :name => "Lord Montague",
+       :age => "53"
+   },
+     :matriarch
+    
+   }
 end
