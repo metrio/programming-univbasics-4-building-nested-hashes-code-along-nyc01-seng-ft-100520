dd8f52201a87dc74ca441e56864c05b96469e4c8
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-21 16:57:14 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -10,19 +10,23 @@ def third_challenge
         :age
         },
       :hero => {
-        #your key/value pairs here
+        :name,
+        :age
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name,
+        :age
         },
       :matriarch => {
-        #your key/value pairs here
+        :name,
+        :age
         },
       :heroine => {
-        #your key/value pairs here
+        :name,
+        :age
         },
       :heroine_friends => []
    }
