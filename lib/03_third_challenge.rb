fe39327a242b0216a2892f6d4271b22c25e9ccfb
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-21 17:02:13 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -27,7 +27,7 @@ def third_challenge
         },
       :heroine => {
         :name => "Juliet",
-        :age => "14",
+        :age => "15",
         :status => "alive"
         },
       :heroine_friends => []
