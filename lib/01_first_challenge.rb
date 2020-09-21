cb7b57ecf4d36aa5127959bca3ef77e0fd80f5e9
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-21 16:44:22 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -22,5 +22,27 @@ def first_challenge
        :attitude => "worried"
      }]
    },
-   
+   :capulet => {
+     :patriarch =>{
+       :name => "Lord Capulet",
+       :age => "50"
+     },
+     :matriarch =>{
+       :name => "Lady Capulet",
+       :age => "51"
+     },
+     :heroine => {
+       :name "Juliet",
+       :age => "15",
+       :status => "alive"
+     },
+     :heroine_friends => [{
+       :name => "Steven",
+       :age => "30",
+       :attitude => 
+     }]
+     }
+     }
+     }
+   }
 end
